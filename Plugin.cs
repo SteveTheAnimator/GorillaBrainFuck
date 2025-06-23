41ab6f6dfc74644e00a0b2695a03d287bd878ab1
SteveCoolFart <103543324+SteveCoolFart@users.noreply.github.com>
2025-06-23 10:02:19 -0600

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using GorillaBrainFuck.Types;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine.InputSystem;
+using System.Reflection;
+
+namespace GorillaBrainFuck
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private string codeInput = string.Empty;
+        private string lastOutput = string.Empty;
+        private bool guiOpen = false;
+        private GUILayoutOption heightOption;
+        private GUILayoutOption expandWidthOption;
+        private Rect guiArea = new Rect(10, 10, 400, 250);
+        private bool isDragging = false;
+        private Vector2 dragOffset = Vector2.zero;
+
+        public void Start() => GorillaTagger.OnPlayerSpawned(Init);
+
+        public void Init()
+        {
+            Debug.Log($"{PluginInfo.Name} initialized.");
+            heightOption = GUILayout.Height(60);
+            expandWidthOption = GUILayout.ExpandWidth(true);
+        }
+
+        public void Update()
+        {
+            if (Keyboard.current.f6Key.wasPressedThisFrame)
+            {
+                guiOpen = !guiOpen;
+            }
+        }
+
+        public async Task Execute(bool res = true)
+        {
+            var input = new StringReader(codeInput);
+            var output = new StringWriter();
+            var interpreter = new Interpreter(input, output);
+
+            if (res)
+            {
+                lastOutput = await interpreter.Execute(codeInput);
+            }
+            else
+            {
+                await interpreter.Execute(codeInput);
+                lastOutput = output.ToString();
+            }
+        }
+
+        public void OnGUI()
+        {
+            if (!guiOpen) return;
+
+            Rect dragHandleRect = new Rect(guiArea.x, guiArea.y, guiArea.width, 20);
+            GUI.Box(dragHandleRect, "");
+
+            if (Event.current.type == EventType.MouseDown && dragHandleRect.Contains(Event.current.mousePosition))
+            {
+                isDragging = true;
+                dragOffset = Event.current.mousePosition - new Vector2(guiArea.x, guiArea.y);
+                Event.current.Use();
+            }
+            else if (Event.current.type == EventType.MouseUp && isDragging)
+            {
+                isDragging = false;
+                Event.current.Use();
+            }
+            else if (Event.current.type == EventType.MouseDrag && isDragging)
+            {
+                guiArea.x = Mathf.Clamp(Event.current.mousePosition.x - dragOffset.x, 0, Screen.width - guiArea.width);
+                guiArea.y = Mathf.Clamp(Event.current.mousePosition.y - dragOffset.y, 0, Screen.height - guiArea.height);
+                Event.current.Use();
+            }
+
+            GUILayout.BeginArea(guiArea);
+            GUILayout.Label("Gorilla BrainFuck Interpreter - Drag to move");
+            codeInput = GUILayout.TextField(codeInput, heightOption, expandWidthOption);
+
+            if (GUILayout.Button("Execute Code [Result]"))
+            {
+                _ = Execute();
+            }
+
+            if (GUILayout.Button("Execute Code [Output]"))
+            {
+                _ = Execute(false);
+            }
+
+            GUILayout.Label("Output:");
+            GUILayout.TextArea(lastOutput, heightOption, expandWidthOption);
+
+            if (GUILayout.Button("Close"))
+            {
+                guiOpen = false;
+            }
+
+            GUILayout.EndArea();
+        }
+    }
+
+    public class PluginInfo
+    {
+        internal const string
+            GUID = "Steve.GorillaBrainFuck",
+            Name = "GorillaBrainFuck",
+            Version = "1.0.0";
+    }
+}
---
+++ Types/Interpreter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorillaBrainFuck.Types
+{
+    public class Interpreter
+    {
+        private byte[] memory;
+        private int pointer;
+        private TextReader input;
+        private TextWriter output;
+        private const int MaxIterations = 1000000;
+
+        public Interpreter(TextReader input = null, TextWriter output = null)
+        {
+            memory = new byte[30000];
+            pointer = 0;
+            this.input = input ?? Console.In;
+            this.output = output ?? Console.Out;
+        }
+
+        public async Task<string> Execute(string code)
+        {
+            StringBuilder result = new StringBuilder();
+            Dictionary<int, int> bracketPairs = PrecomputeBracketPairs(code);
+            int iterationCount = 0;
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                if ("<>+-.,[]".IndexOf(code[i]) != -1)
+                {
+                    if (++iterationCount > MaxIterations)
+                        throw new InvalidOperationException("stop looping you not kind person");
+                }
+                else
+                {
+                    continue;
+                }
+
+                try
+                {
+                    switch (code[i])
+                    {
+                        case '>': pointer = (pointer + 1) % memory.Length; break;
+                        case '<': pointer = (pointer - 1 + memory.Length) % memory.Length; break;
+                        case '+': memory[pointer]++; break;
+                        case '-': memory[pointer]--; break;
+                        case '.':
+                            char outputChar = (char)memory[pointer];
+                            await output.WriteAsync(outputChar);
+                            result.Append(outputChar);
+                            break;
+                        case ',':
+                            char[] buffer = new char[1];
+                            int read = await input.ReadAsync(buffer, 0, 1);
+                            if (read > 0)
+                                memory[pointer] = (byte)buffer[0];
+                            else
+                                memory[pointer] = 0;
+                            break;
+                        case '[':
+                            if (memory[pointer] == 0 && bracketPairs.TryGetValue(i, out int jumpTo))
+                            {
+                                i = jumpTo;
+                                iterationCount--;
+                            }
+                            break;
+                        case ']':
+                            if (memory[pointer] != 0 && bracketPairs.TryGetValue(i, out int jumpBack))
+                            {
+                                i = jumpBack;
+                                iterationCount--;
+                            }
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException($"error '{code[i]}' at position {i}: {ex.Message}");
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private Dictionary<int, int> PrecomputeBracketPairs(string code)
+        {
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+            Stack<int> openBrackets = new Stack<int>();
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (code[i] == '[')
+                {
+                    openBrackets.Push(i);
+                }
+                else if (code[i] == ']')
+                {
+                    if (openBrackets.Count == 0)
+                        throw new InvalidOperationException("missing opening bracket");
+
+                    int openPos = openBrackets.Pop();
+                    pairs[openPos] = i;
+                    pairs[i] = openPos;
+                }
+            }
+
+            if (openBrackets.Count > 0)
+                throw new InvalidOperationException("missing closing bracket");
+
+            return pairs;
+        }
+
+        public void Reset()
+        {
+            Array.Clear(memory, 0, memory.Length);
+            pointer = 0;
+        }
+
+        public async Task<string> ExecuteFile(string filePath)
+        {
+            string code = await File.ReadAllTextAsync(filePath);
+            return await Execute(code);
+        }
+    }
+}
